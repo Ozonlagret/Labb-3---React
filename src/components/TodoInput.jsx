5b4837065485b1f64fe3e84293f3df58c19f17eb
Tim D <tim.drejhede@gmail.com>
2025-03-25 11:07:55 +0100

    Update TodoInput.jsx
    Removed function and put the code from it directly in the return statement with an arrow function instead.

--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -3,24 +3,24 @@ import { useState } from "react";
 function TodoInput({addTodo}) {
     const[text, setText] = useState("");
 
-    function handleAdd() {
-        if (text.trim() !== "") {
-            addTodo(text);
-            setText("");
-        }
-    };
-
     return (
         <div>
             <input type="text" value={text} className="left"
             onChange={(e) => setText(e.target.value)}
             />
 
-            <button className="right" onClick={handleAdd}>Lägg till</button>
+            <button className="right" 
+            onClick={() => {
+                if (text.trim() !== "") 
+                {
+                    addTodo(text);
+                    setText("");
+                }
+                }} >Lägg till</button>
         </div>
     )
 
 
 }
 
-export default TodoInput; 
+export default TodoInput;  
