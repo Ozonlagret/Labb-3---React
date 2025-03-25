2503052c0b1b342f2a4dbf1214b55da0fefaf02f
Tim D <tim.drejhede@gmail.com>
2025-03-25 11:10:08 +0100

    Update TodoItem.jsx
    Gave the items a checkbox that lets the user create a line-through to signal completion of the respective to-do list item.

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,11 +1,12 @@
 function TodoItem({todo, toggle, removeTodo}) {
     return (
-        <div>
-            <span onClick ={() => toggle(todo.id)}
-                className={todo.completed ? 'done' : 'not-done'}>
-                {todo.text}
-            </span>
+        <div className="todoItem">
+            <input 
+            type="checkbox"
+            checked={todo.completed}
+            onChange={() => {toggle(todo.id)}}></input>
 
+            <span id="itemText" className={todo.completed ? 'done' : 'not-done'}>{todo.text}</span>
             <button className="removeButton" onClick={() => removeTodo(todo.id)}>Ta bort</button>
         </div>
     )
